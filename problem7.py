4166dfa8a59836f5f45acbc38923c7a1d08a344f
nidhinp <nidhinp.412@gmail.com>
2013-11-18 11:39:35 +0530

    fist commit

---
+++ problem7.py
@@ -0,0 +1,23 @@
+import sys
+
+def write_file(lines, newfiles):
+	f = open('%d.txt' %newfiles, 'w')
+	for line in lines:
+		f.write(line)
+	f.close()
+
+def split(count, filename):
+	f = open(filename)
+	line_list = f.readlines()
+	newfiles = len(line_list) / count
+	i = 0
+	while newfiles > 0:
+		write_file(line_list[i:i+count], newfiles)
+		i += count
+		newfiles -= 1 	
+		
+
+if len(sys.argv) < 3:
+	print 'usage python problem7.py number filename'
+else:
+	split(int(sys.argv[1]), sys.argv[2])
